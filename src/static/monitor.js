6807f90ee65b212c8416b2bcf33bcd7c2a70d06e
Sergey <sergey.kalitin@gmail.com>
2016-09-16 00:01:12 +0000

    networks

--- src/static/monitor.js
+++ src/static/monitor.js
@@ -7,19 +7,35 @@ function processResponse(data) {
 
     for (var i = 0; i < containers.length; i++) {
         var container = containers[i];
-
+        var labels = container.Labels;
+        var networkNames = Object.keys(container.NetworkSettings.Networks);
         var details =
             "<b>Name:</b> " + container.Names[0] +
             "<br><b>Status:</b> " + container.Status +
             "<br><b>State:</b> " + container.State +
-            "<br><b>Image:</b> " + container.Image;
+            "<br><b>Image:</b> " + container.Image +
+            "<br><b>Networks:</b> " + networkNames;
 
+        var colorCode = container.Names[0];
+        if(labels.tenant)
+            colorCode = labels.tenant;
+        
+        var label = container.Names[0].substring(1);
+        if(label.includes('backups-sync'))
+            label = '...backups-sync';
+        else if(label.includes('backups-unpacking'))
+            label = '...backups-unpacking';
+        else if(label.includes('restore'))
+            label = '...restore';
+        else if(label.includes('provision'))
+            label = '...provision';
+                                          
         var node = {
             id: container.Id,
-            label: container.Names[0],
+            label: label,
             title: details,
             color: Please.make_color({
-                from_hash: container.Names[0]
+                from_hash: colorCode
             })
         };
 
